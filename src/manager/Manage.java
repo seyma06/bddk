734bf218e0b3a5b379a70e22a35eec88d6bade07
Birkan <birkanmutluer@gmail.com>
2016-07-11 09:23:36 +0300

    id'yi kullanicinin bilmesine gerek yok ki
    ben sadece ekleyecegim seyi ekleyim, id'yi her manager kendi bilsin

--- src/manager/Manage.java
+++ src/manager/Manage.java
@@ -4,7 +4,7 @@ package manager;
  * Created by Administrator on 29.06.2016.
  */
 public interface Manage {
-    public void add(int id,String name);
+    public void add(String name);
     public void view(int id);
     public void remove(int id);
 
